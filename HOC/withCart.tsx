d033c74ab80fa43fba348a2fd7b9f063523b8942
javierscode <javihp79@gmail.com>
2021-11-21 11:00:42 +0100

    :bug: use useEffect to check the current cart

--- HOC/withCart.tsx
+++ HOC/withCart.tsx
@@ -1,25 +1,26 @@
 // HOC/withAuth.jsx
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import ShopContext from '../context/ShopContext'
+import CartItem from '../intefaces/ cart'
 
 export function withCart<P>(Page: NextPage<P>) {
   return function WrappedPage(props: P) {
     const Router = useRouter()
-    const { cart } = useContext(ShopContext)
+    const { cart, cartInitiated } = useContext(ShopContext)
 
-    if (typeof window !== 'undefined') {
-      // If there is no product in the shopping cart we redirect to "/" page.
-      if (cart.length === 0) {
+    const [renderPage, setRenderPage] = useState(false)
+
+    useEffect(() => {
+      if (cart.length == 0 && cartInitiated) {
         Router.replace('/')
-        return null
       }
-      // If we have any product in the shopping cart we just render the component that was passed with all its props
-      return <Page {...props} />
-    }
+      if (cart.length > 0 && cartInitiated) {
+        setRenderPage(true)
+      }
+    }, [Router, cart.length, cartInitiated])
 
-    // If we are on server, return null
-    return null
+    return renderPage ? <Page {...props} /> : null
   }
 }
